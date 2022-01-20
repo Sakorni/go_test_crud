d48d14c4274a90127df6927313375f823da0081f
Sakorni <ssakorni@gmail.com>
2022-01-20 22:12:26 +0300

    Added required tag to model

--- models/user.go
+++ models/user.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type User struct {
-	ID uint `json:"id" db:"id"`
-	Firstname string `json:"first_name" db:"firstname"`
-	Lastname string `json:"last_name" db:"lastname"`
-	Email string `json:"email" db:"email"`
-	Age uint `json:"age" db:"age"`
-	Created time.Time `json:"created_at" db:"created_at"`
+	ID        uint      `json:"id" db:"id"`
+	Firstname string    `json:"first_name" db:"firstname" binding:"required" `
+	Lastname  string    `json:"last_name" db:"lastname" binding:"required"`
+	Email     string    `json:"email" db:"email" binding:"required"`
+	Age       uint      `json:"age" db:"age" binding:"required"`
+	Created   time.Time `json:"created_at" db:"created_at"`
 }
